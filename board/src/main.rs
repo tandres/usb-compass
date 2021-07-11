754e7bf79d6f27636a3b91a2b280dac1f8a77bc7
Trenton Andres <trenton.andres@gmail.com>
2021-07-11 17:39:38 -0500

    blink red light with asm delays

--- board/src/main.rs
+++ board/src/main.rs
@@ -4,14 +4,26 @@
 //use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
+use stm32f3xx_hal as hal;
+
+use cortex_m::asm::delay;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 
+use hal::pac;
+use hal::prelude::*;
+
 #[entry]
 fn main() -> ! {
+    let peris = pac::Peripherals::take().unwrap();
+    let mut rcc = peris.RCC.constrain();
+    let mut gpioe = peris.GPIOE.split(&mut rcc.ahb);
 
+    let mut red_led = gpioe.pe13.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
     hprintln!("Hello!").unwrap();
 
     loop {
+        red_led.toggle().unwrap();
+        delay(8000000)
     }
 }
