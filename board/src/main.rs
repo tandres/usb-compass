898b35c5739de0b989c148d494600f7e656587bd
Trenton Andres <trenton.andres@gmail.com>
2021-07-11 14:29:55 -0500

    hello world on board working

--- board/src/main.rs
+++ board/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+//use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+
+#[entry]
+fn main() -> ! {
+
+    hprintln!("Hello!").unwrap();
+
+    loop {
+    }
 }
