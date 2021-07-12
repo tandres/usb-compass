280ae3c1f0aa25433f27975133a234dc7ca7b82d
Trenton Andres <trenton.andres@gmail.com>
2021-07-11 21:13:57 -0500

    libusb list

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,15 @@
+use libusb;
+
 fn main() {
-    println!("Hello, world!");
+    let mut context = libusb::Context::new().unwrap();
+
+    for mut device in context.devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+
+        println!("Bux {:03} Device {:03} ID {:04x}:{:04x}",
+            device.bus_number(),
+            device.address(),
+            device_desc.vendor_id(),
+            device_desc.product_id());
+    }
 }
