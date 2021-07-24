8cf353616929a828665413f6f243bac95404c9e3
Trenton Andres <trenton.andres@gmail.com>
2021-07-24 07:07:19 -0500

    use slip instead of cobs

--- common/src/link.rs
+++ common/src/link.rs
@@ -1,15 +1,12 @@
 use serde_cbor::error::Error as CborError;
-use cobs::{max_encoding_length, encode, decode};
+use serial_line_ip::{Encoder, Decoder, Error as SlipError};
 use crate::message::Message;
 use static_assertions::const_assert;
 
 #[derive(Debug)]
 pub enum LinkError {
     Cbor(CborError),
-    PacketTooLarge,
-    PacketNoStop,
-    Cobs,
-    DestTooSmall,
+    Slip(SlipError),
 }
 
 impl From<CborError> for LinkError {
@@ -18,203 +15,61 @@ impl From<CborError> for LinkError {
     }
 }
 
+impl From<SlipError> for LinkError {
+    fn from(e: SlipError) -> LinkError {
+        LinkError::Slip(e)
+    }
+}
+
 type Result<T> = core::result::Result<T, LinkError>;
 
 const MAX_PACKET_SIZE: usize = Message::MAX_SIZE;
 const_assert!(MAX_PACKET_SIZE < u16::MAX as usize);
-struct Packet<'a> {
-    size: u16,
-    write: u16,
-    // read: u16,
-    data: &'a mut [u8],
+
+pub struct Link {
+    decoder: Decoder,
+    scratch_offset: usize,
+    scratch: [u8; MAX_PACKET_SIZE],
 }
 
-impl<'a> Packet<'a> {
-    const START: u8 = 0x01;
-    const STOP: u8 = 0x00;
-    const HEADER_SIZE: usize = 3;
-    fn new(data: &'a mut [u8]) -> Packet {
-        Packet {
-            size: 0,
-            write: 0,
-            // read: 0,
-            data,
+impl Link {
+    pub fn new() -> Link {
+        Link {
+            decoder: Decoder::new(),
+            scratch_offset: 0,
+            scratch: [0u8; MAX_PACKET_SIZE],
         }
     }
 
-    fn new_with_size(size: u16, data: &'a mut [u8]) -> Result<Self> {
-        let mut packet = Self::new(data);
-        packet.set_header(size)?;
-        packet.set_write(Self::HEADER_SIZE as u16);
-        Ok(packet)
+    pub fn encode(&mut self, msg: &Message, output: &mut [u8]) -> Result<usize> {
+        let mut encoder = Encoder::new();
+        let mut buf = [0u8; MAX_PACKET_SIZE];
+        let size = msg.write_bytes(&mut buf)?;
+        let mut totals = encoder.encode(&buf[..size], output)?;
+        totals += encoder.finish(&mut output[totals.written..])?;
+        Ok(totals.written)
     }
 
-    fn set_header(&mut self, size: u16) -> Result<()> {
-        if size as usize > MAX_PACKET_SIZE - Self::HEADER_SIZE {
-            return Err(LinkError::PacketTooLarge);
-        }
-        self.size = size;
-        self.buffer_write_start();
-        self.buffer_write_size(size);
-        Ok(())
-    }
-
-    fn set_write(&mut self, write: u16) {
-        self.write = write;
-    }
-
-    fn buffer_write_stop(&mut self) {
-        self.data[self.write as usize] = Self::STOP;
-        self.write += 1;
-        self.size += 1;
-    }
-
-    fn buffer_write_start(&mut self) {
-        self.data[0] = Self::START;
-    }
-
-    fn buffer_write_size(&mut self, size: u16) {
-        let size = size.to_le_bytes();
-        self.data[1] = size[0];
-        self.data[2] = size[1];
-    }
-
-    pub fn get_size(&self) -> usize {
-        self.size as usize
-    }
-
-    fn push(&mut self, data: u8) -> bool {
-        self.data[self.write as usize] = data;
-        self.write += 1;
-        self.write == self.size
-    }
-
-    fn decode_data(&self, output: &mut [u8]) -> Result<usize> {
-        if output.len() < self.size as usize - Self::HEADER_SIZE {
-            return Err(LinkError::DestTooSmall);
-        }
-        match decode(&self.data[Self::HEADER_SIZE..self.size as usize], output) {
-            Ok(size) => Ok(size),
-            Err(_) => Err(LinkError::Cobs),
-        }
-    }
-
-    fn build(input_data: &[u8], packet_data: &'a mut [u8]) -> Result<Packet<'a>> {
-        if input_data.len() > MAX_PACKET_SIZE || max_encoding_length(input_data.len()) > MAX_PACKET_SIZE {
-            return Err(LinkError::PacketTooLarge);
-        }
-        let mut packet = Packet::new(packet_data);
-        let size = encode(input_data, &mut packet.data[Self::HEADER_SIZE..]) + Self::HEADER_SIZE;
-        packet.set_header(size as u16)?;
-        packet.set_write(size as u16);
-        packet.buffer_write_stop();
-        Ok(packet)
-    }
-
-    // fn to_bytes(self) -> (usize, [u8; MAX_PACKET_SIZE]) {
-    //     (self.size as usize, self.data)
-    // }
-}
-
-enum RxState<'a> {
-    Idle,
-    Start,
-    Size(u8),
-    Data(Packet<'a>),
-    Stop(Packet<'a>),
-}
-
-pub struct Link<'a> {
-    rx_state: Option<RxState<'a>>,
-    data: Option<&'a mut [u8]>,
-}
-
-impl<'a> Link<'a> {
-    pub fn new(data: &'a mut [u8]) -> Self {
-        Self {
-            rx_state : Some(RxState::Idle),
-            data: Some(data),
-        }
-    }
-
-    pub fn encode(&mut self, message: &Message) -> Result<(usize, [u8; Message::MAX_SIZE])> {
-        let mut buf = [0u8; Message::MAX_SIZE];
-        let mut input_buf = [0u8; Message::MAX_SIZE];
-        let size = message.write_bytes(&mut input_buf)?;
-        let packet = Packet::build(&input_buf, &mut buf[..size])?;
-        Ok((packet.get_size(), buf))
-    }
-
-    pub fn push(&mut self, data: u8) -> Result<Option<Message>> {
-        if let Some(packet) = self.turn(data)? {
-            let mut buf = [0u8; Message::MAX_SIZE];
-            let size = packet.decode_data(&mut buf[..])?;
-            Ok(Some(Message::from_bytes(&buf[..size])?))
-        } else {
-            Ok(None)
-        }
-    }
-
-    pub fn push_slice(&mut self, data: &[u8]) -> Result<(usize, Option<Message>)> {
-        let mut i = 0;
-        for b in data {
-            let res = self.push(*b)?;
-            i += 1;
-            if res.is_some() {
-                return Ok((i, res));
-            }
-        }
-        Ok((i, None))
-    }
-
-    fn turn(&mut self, data: u8) -> Result<Option<Packet>> {
-        use RxState::*;
-        let mut res = Ok(None);
-        let next_state = match self.rx_state.take().unwrap() {
-            Idle => {
-                if Packet::START == data {
-                    Start
-                } else {
-                    Idle
-                }
-            }
-            Start => {
-                Size(data)
-            }
-            Size(lb) => {
-                let buf = self.data.take().unwrap();
-                let size = u16::from_le_bytes([lb, data]);
-                match Packet::new_with_size(size, buf) {
-                    Ok(packet) => Data(packet),
-                    Err(e) => {
-                        res = Err(e);
-                        Idle
+    pub fn decode(&mut self, buf: &[u8]) -> Result<(usize, Option<Message>)> {
+        let (read, packet, present) = self.decoder.decode(buf, &mut self.scratch[self.scratch_offset..])?;
+        self.scratch_offset += packet.len();
+        let mut res = None;
+        if present {
+            if packet.len() != 0 {
+                match Message::from_bytes(&self.scratch[..self.scratch_offset]) {
+                    Ok(msg) => {
+                        res = Some(msg);
+                    }
+                    Err(_e) => {
+                        //Bad packet, report no packet with data read
+                        //println!("Bad packet: {}", _e);
                     }
                 }
+                self.scratch_offset = 0;
             }
-            Data(mut packet) => {
-                if packet.push(data) {
-                    Stop(packet)
-                } else {
-                    Data(packet)
-                }
-            }
-            Stop(packet) => {
-                if Packet::STOP == data {
-                    res = Ok(Some(packet));
-                    Idle
-                } else {
-                    res = Err(LinkError::PacketNoStop);
-                    Idle
-                }
-            }
-        };
-        self.rx_state = Some(next_state);
-        return res;
-    }
+        }
 
-    fn reset(&mut self, data: &'a mut [u8]) {
-        self.data = Some(data);
+        Ok((read, res))
     }
 }
 
@@ -224,64 +79,44 @@ mod test {
     use super::{Link, MAX_PACKET_SIZE};
 
     fn echo_test(msg: Message) {
-        let mut working_buf = [0u8; MAX_PACKET_SIZE];
-        let mut link = Link::new(&mut working_buf);
-        let (size, buf) = link.encode(&msg).unwrap();
-        let (used, rx) = link.push_slice(&buf[..size]).unwrap();
-        assert_eq!(used, size);
+        let mut buf = [0u8; MAX_PACKET_SIZE];
+        let mut link = Link::new();
+        let size = link.encode(&msg, &mut buf).unwrap();
+        let (_size, rx) = link.decode(&buf[..size]).unwrap();
         assert_eq!(msg, rx.unwrap());
     }
 
-    #[test]
-    fn say_hello() {
-        let msg = Message::Hello;
-        let mut working_buf = [0u8; MAX_PACKET_SIZE];
-        let mut link = Link::new(&mut working_buf);
-        let (size, buf) = link.encode(&msg).unwrap();
-        for b in &buf[..size] {
-            let res: Option<Message> = link.push(*b).unwrap();
-            if let Some(rx) = res {
-                assert_eq!(msg, rx);
+    fn multi_message_encode(msgs: &Vec<Message>, link: &mut Link) -> (usize, Vec<u8>) {
+        let mut buf = vec![0u8; msgs.len() * MAX_PACKET_SIZE];
+        println!("Buffer size: {}", buf.len());
+        let mut offset = 0;
+        for msg in msgs {
+            let size = link.encode(&msg, &mut buf[offset..]).unwrap();
+            println!("SIZE: {} Offset: {} Buf: {:?}", size, offset, &buf[offset..size+offset]);
+            offset += size;
+        }
+        (offset, buf)
+    }
+
+    fn multi_message_decode(msgs: &Vec<Message>, link: &mut Link, buf: &[u8]) {
+        let mut msgindex = 0;
+        let mut offset = 0;
+        loop {
+            let (size, rx) = link.decode(&buf[offset..]).unwrap();
+            println!("Buf {:?}", &buf[offset..]);
+            if size == 0 {
                 break;
             }
+            if rx.is_some() {
+                assert_eq!(msgs[msgindex], rx.unwrap());
+                msgindex += 1;
+            }
+            offset += size;
         }
     }
 
     #[test]
-    fn repeat_msg() {
-        let msg = Message::Hello;
-        let mut working_buf = [0u8; MAX_PACKET_SIZE];
-        let mut link = Link::new(&mut working_buf);
-        let (size, buf) = link.encode(&msg).unwrap();
-        let (used, rx) = link.push_slice(&buf[..size]).unwrap();
-        assert_eq!(used, size);
-        assert_eq!(msg, rx.unwrap());
-        link.reset(&mut working_buf);
-        let (used, rx) = link.push_slice(&buf[..size]).unwrap();
-        assert_eq!(used, size);
-        assert_eq!(msg, rx.unwrap());
-    }
-
-    // #[test]
-    // fn cobs_test() {
-    //     let msg = Message::Hello;
-    //     let mut buf = [0u8; 100];
-    //     let size = msg.write_bytes(&mut buf).unwrap();
-    //     println!("Size: {} buf: {:?}", size, &buf[..size]);
-    //     let mut output_enc = [0u8; 100];
-
-    //     let outsize = encode(&buf[..size], &mut output_enc);
-    //     println!("Size: {} buf: {:?}", outsize, &output_enc[..outsize]);
-    //     let mut output_dec = [0u8; 100];
-
-    //     let outsize = decode(&output_enc[..outsize], &mut output_dec).unwrap();
-    //     println!("Size: {} buf: {:?}", outsize, &output_dec[..outsize]);
-
-
-    // }
-
-    #[test]
-    fn say_hello_slice() {
+    fn say_hello() {
         echo_test(Message::Hello);
     }
 
@@ -289,4 +124,43 @@ mod test {
     fn large_buffer() {
         echo_test(Message::log(b"Hello, World"))
     }
+
+    #[test]
+    fn multiple_message() {
+        let msgs = vec![Message::Hello, Message::Hello];
+        let mut link = Link::new();
+        let (length, buf) = multi_message_encode(&msgs, &mut link);
+        multi_message_decode(&msgs, &mut link, &buf[..length]);
+    }
+
+    #[test]
+    fn bad_message() {
+        let mut offset = 0;
+        let input = vec![
+            192, 111, 73, 111, 118, 118, 111, 192,
+            192, 101, 72, 101, 108, 108, 111, 192];
+        let mut link = Link::new();
+        let (sz, msg) = link.decode(&input).unwrap();
+        offset += sz;
+        assert!(msg.is_none());
+        let (sz, msg) = link.decode(&input[offset..]).unwrap();
+        offset += sz;
+        assert!(msg.is_none());
+        let (_, msg) = link.decode(&input[offset..]).unwrap();
+        assert_eq!(Message::Hello, msg.unwrap());
+    }
+
+    #[test]
+    fn partial_decodes() {
+
+        let input = vec![192, 101, 72, 101, 108, 108, 111, 192];
+        let mut link = Link::new();
+        let (sz, msg) = link.decode(&input[..2]).unwrap();
+        assert!(msg.is_none());
+        assert_eq!(sz, 2);
+        let (sz, msg) = link.decode(&input[sz..]).unwrap();
+        assert!(msg.is_some());
+        assert_eq!(sz, 6);
+    }
+
 }
